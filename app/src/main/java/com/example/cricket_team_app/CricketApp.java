d0395bdb16f02a3ccf3b4c7369b5ee96b5befad6
RishiRapvi <90780199+RishiRapvi@users.noreply.github.com>
2022-09-21 12:33:30 -0500

    Added classes

---
+++ app/src/main/java/com/example/cricket_team_app/CricketApp.java
@@ -0,0 +1,51 @@
+package com.example.cricket_team_app;
+
+public class CricketApp {
+    private String name;
+    private String position;
+    private String desc;
+    private int imageResourceID;
+
+    public CricketApp(String name, String position, String desc, int imageResourceID) {
+        this.name = name;
+        this.position = position;
+        this.desc = desc;
+        this.imageResourceID = imageResourceID;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPosition() {
+        return position;
+    }
+
+    public void setPosition(String position) {
+        this.position = position;
+    }
+
+    public String getDesc() {
+        return desc;
+    }
+
+    public void setDesc(String desc) {
+        this.desc = desc;
+    }
+
+    public int getImageResourceID() {
+        return imageResourceID;
+    }
+
+    public void setImageResourceID(int imageResourceID) {
+        this.imageResourceID = imageResourceID;
+    }
+
+
+
+}
+
